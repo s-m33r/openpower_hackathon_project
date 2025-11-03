cdd79ec814fb5528b63b8d23084a1559436c4406
s-m33r <112017240+s-m33r@users.noreply.github.com>
2025-11-03 19:38:07 +0530

    removed openframe files

--- verilog/dv/cocotb/hello_world/hello_world.py
+++
@@ -1,9 +0,0 @@
-from cocotb_includes import test_configure
-from cocotb_includes import report_test
-import cocotb
-
-@cocotb.test()
-@report_test
-async def hello_world(dut):
-    caravelEnv = await test_configure(dut,timeout_cycles=9373)
-    cocotb.log.info("Hello World")
